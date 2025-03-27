f939ee14b3c54ab87cf3d11f008fa11ab729b920
Alan Huynh <hdmquan@outlook.com>
2025-03-27 07:27:42 +0000

    refactor(viz): Move visualisation functions to separate file

--- src/notebooks/eda/visualisation.py
+++ src/notebooks/eda/visualisation.py
@@ -7,72 +7,7 @@ from datetime import datetime
 import torch
 from src.utils import PATH
 from src.data.module import MPIDataModule
-
-
-def plot_layer(x, lev, num_channels=6, titles=None):
-    rows = (num_channels + 1) // 2
-    if titles is None:
-        titles = [f"Bin {i+1}" for i in range(num_channels)]
-
-    fig = make_subplots(
-        rows=rows,
-        cols=2,
-        subplot_titles=titles,
-    )
-
-    for i in range(num_channels):
-        # :D
-        row = i // 2 + 1
-        col = i % 2 + 1
-
-        fig.add_trace(
-            go.Heatmap(
-                z=x[0, i, lev, :, :],
-                colorscale="RdBu",
-                reversescale=True,
-                # zmin=-1,
-                # zmax=1,
-            ),
-            row=row,
-            col=col,
-        )
-
-    fig.update_layout(
-        height=800,
-    )
-
-    fig.show()
-
-
-def plot_long_cut(x, long, num_channels=6, titles=None):
-    rows = (num_channels + 1) // 2
-    if titles is None:
-        titles = [f"Bin {i+1}" for i in range(num_channels)]
-
-    fig = make_subplots(rows=rows, cols=2, subplot_titles=titles)
-
-    for i in range(num_channels):
-        # :D
-        row = i // 2 + 1
-        col = i % 2 + 1
-
-        fig.add_trace(
-            go.Heatmap(
-                z=x[0, i, :, :, long],
-                colorscale="RdBu",
-                reversescale=True,
-                # zmin=-1,
-                # zmax=1,
-            ),
-            row=row,
-            col=col,
-        )
-
-    fig.update_layout(
-        height=800,
-    )
-
-    fig.show()
+from src.utils.plotting import plot_layer, plot_long_cut
 
 
 # %% Data
